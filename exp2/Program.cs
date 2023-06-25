67f5dd1f5df2ffb31029a4fb71262da881981883
LiyaMart <aleka2021@mail.ru>
2023-06-25 21:25:15 +0300

    tast2

---
+++ exp2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+
+Console.WriteLine("Введите номер дня недели");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1) Console.WriteLine("monday");
+if (number == 2) Console.WriteLine("tuesday");
+if (number == 3) Console.WriteLine("wendsday");
+if (number == 4) Console.WriteLine("tusday");
+if (number == 5) Console.WriteLine("friday");
+if (number == 6) Console.WriteLine("sunday");
+if (number == 7) Console.WriteLine("saturday");
+if (number > 7) Console.WriteLine("не является номером дня недели");
+if (number < 1) Console.WriteLine("не является номером дня недели");
