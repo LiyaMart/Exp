a33d2668fae4571429029c085325d8778a0c6280
LiyaMart <aleka2021@mail.ru>
2023-06-25 21:48:52 +0300

    tast4

---
+++ exp4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает трехзначное число и 
+// на выходе показывает последнюю цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number<1000 && number>99)
+{
+    Console.WriteLine(number%10);
+}
+else
+{
+    Console.WriteLine("Вы ввели число отличное от трехзначного!");
+}
