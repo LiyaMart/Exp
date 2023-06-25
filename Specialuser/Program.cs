214bd8c180036310d8757e8b907b59fe4277ddd8
LiyaMart <aleka2021@mail.ru>
2023-06-25 19:25:44 +0300

    Specialuser

---
+++ Specialuser/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите Ваше имя ");
+string username=Console.ReadLine();
+if (username.ToLower() == "лилия")
+{  
+    Console.Write("Здравствуй, Солнышко!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+    Console.Write("!");
+}
