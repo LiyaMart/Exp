503a81d8d7bdfc2036d163390b95a145f19606d8
LiyaMart <aleka2021@mail.ru>
2023-06-25 20:51:49 +0300

    tast0

---
+++ exp0/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая на вход принимат число и выдает его квадрат (число уноженное на самл себя)
+
+Console.Write("Введите число ");
+long number =Convert.ToInt64(Console.ReadLine());
+long square = number*number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
