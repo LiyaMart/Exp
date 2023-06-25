c54b6cb2f65b3520e945146d4fa84b7cbe6f064d
LiyaMart <aleka2021@mail.ru>
2023-06-25 21:16:58 +0300

    tast1

---
+++ exp1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, 
+// является ли первое число квадратом второго
+
+Console.WriteLine("Введите первое число ");
+int number_1; 
+number_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int number_2;
+number_2 = Convert.ToInt32(Console.ReadLine());
+
+if (number_2 == number_1*number_1)
+{
+    Console.WriteLine("true");
+}
+else
+{
+    Console.WriteLine("false");
+}
