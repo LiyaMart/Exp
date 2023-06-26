8a0ead633c674a9a8433f2d65cf446176c3f8882
LiyaMart <aleka2021@mail.ru>
2023-06-26 09:17:50 +0300

    tast8

---
+++ exp8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N), и выдает, а на выходе показывает все
+// все четные числа от 1 до N.
+
+Console.Write("Введиете натуральное число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n<=0) Console.WriteLine("Натуральное число, please!");
+
+int count = 1;
+
+while (count<=n)
+{
+    if (count % 2 == 0)
+    Console.Write($"{count}, ");
+    count++;
+}
