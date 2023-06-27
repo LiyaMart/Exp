f165926794c225bbf7a75a81ce1b4e543087b3f3
LiyaMart <aleka2021@mail.ru>
2023-06-27 14:00:05 +0300

    tast12

--- exp12/Program.cs
+++ exp12/Program.cs
@@ -3,12 +3,12 @@
 Console.WriteLine("Введите целое число ");
 int number=Convert.ToInt32(Console.ReadLine());
 
-bool Multiplicity(int num)
+bool Multiplicity(int num, int num1, int num2)
 {
-   return num % 7 == 0 && num % 23 == 0;
+   return num % num1 == 0 && num % num2 == 0;
 }
 
-bool multiplicity = Multiplicity(number);
+bool multiplicity = Multiplicity(number, 7, 23);
 string resultStr = multiplicity ? "кратно" : "не кратно";
 
 Console.WriteLine ($"{number} - {resultStr}");
