8d31b9b76161476e264aca8cf0c31dc26e0eba97
LiyaMart <aleka2021@mail.ru>
2023-06-27 13:35:38 +0300

    tast11

---
+++ exp11/Program.cs
@@ -0,0 +1,17 @@
+﻿// Нипишите программу, которая будет принимать на вход два чилса и выводить, является ли первое число 
+// кратным второму. Если 1 не кратно числу 2, то программа выводить остаток от деления.
+
+Console.WriteLine("Ввдите первое число ");
+int firstNum=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число ");
+int secondNum=Convert.ToInt32(Console.ReadLine());
+
+int Multiplicity(int num1, int num2)
+{
+    return num1 % num2;
+}
+
+int multiplicity = Multiplicity(firstNum, secondNum);
+if (multiplicity == 0) Console.WriteLine("кратное");
+else Console.WriteLine($"не кратно, остаток от деления равен {multiplicity}");
