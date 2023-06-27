7ca27ef7ea127e05015fdd70cb0d1915f8302584
LiyaMart <aleka2021@mail.ru>
2023-06-27 12:56:30 +0300

    tast9

--- exp9/Program.cs
+++ exp9/Program.cs
@@ -1,17 +1,17 @@
 ﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую
 // цифру числа.
 
+int MaxDigit(int num)
+{
+   int firstDigit = num/10;
+   int secondDigit = num%10;
+   if (firstDigit>secondDigit) return firstDigit;
+   return secondDigit;    
+}
+
 int number = new Random().Next(10,100);
 Console.WriteLine($"Случайоное число из отрезка [10, 99] - {number}");
 
-int firstDigit = number/10;
-int secondDigit = number%10;
+int maxDigit = MaxDigit(number);
 
-if (firstDigit>secondDigit) 
-{
-    Console.WriteLine($"Нибольшая цифра числа равна {firstDigit}");
-}
-else
-{
-    Console.WriteLine($"Нибольшая цифра числа равна {secondDigit}");
-}
+Console.WriteLine($"Нибольшая цифра числа равна {maxDigit}");
