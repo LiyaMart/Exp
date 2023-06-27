1d5c0f30f23d2c9e22622a8df04a37487c78a573
LiyaMart <aleka2021@mail.ru>
2023-06-27 12:42:22 +0300

    tast9

---
+++ exp9/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую
+// цифру числа.
+
+int number = new Random().Next(10,100);
+Console.WriteLine($"Случайоное число из отрезка [10, 99] - {number}");
+
+int firstDigit = number/10;
+int secondDigit = number%10;
+
+if (firstDigit>secondDigit) 
+{
+    Console.WriteLine($"Нибольшая цифра числа равна {firstDigit}");
+}
+else
+{
+    Console.WriteLine($"Нибольшая цифра числа равна {secondDigit}");
+}
