a15abfc152a680944352c2705d88a0e268846bd9
LiyaMart <aleka2021@mail.ru>
2023-06-27 14:09:59 +0300

    tast13

---
+++ exp13/Program.cs
@@ -0,0 +1,16 @@
+﻿// Нипишите программу, которая принимает на вход два чилса и проверяет, является ли одно число квадратом
+// другого.
+
+Console.Write("Ввидите первое число ");
+int number1=Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Ввидите второе число ");
+int number2=Convert.ToInt32(Console.ReadLine());
+
+bool Squ(int num1, int num2)
+{
+    return num1 * num1 == num2 || num2 * num2 == num1;
+}
+
+bool squ = Squ(number1, number2);
+Console.WriteLine(squ ? "является" : "не является");
