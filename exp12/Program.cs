d881db15b36e2ca7f0a3ae7b58a5b114854a043d
LiyaMart <aleka2021@mail.ru>
2023-06-27 13:56:37 +0300

    tast12

---
+++ exp12/Program.cs
@@ -0,0 +1,14 @@
+﻿// Нипишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23. 
+
+Console.WriteLine("Введите целое число ");
+int number=Convert.ToInt32(Console.ReadLine());
+
+bool Multiplicity(int num)
+{
+   return num % 7 == 0 && num % 23 == 0;
+}
+
+bool multiplicity = Multiplicity(number);
+string resultStr = multiplicity ? "кратно" : "не кратно";
+
+Console.WriteLine ($"{number} - {resultStr}");
