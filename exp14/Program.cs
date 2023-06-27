9408cca0648f7b8203b15ba56ba65bdcf7e9ad9a
LiyaMart <aleka2021@mail.ru>
2023-06-27 17:08:54 +0300

    tast14

---
+++ exp14/Program.cs
@@ -0,0 +1,18 @@
+﻿// Наипшите программу, которая принимает на вход трехзначное чило и на выходе показывет вторую цифру
+// этого числа.
+
+Console.Write("Введите трехзначное число ");
+int number=Convert.ToInt32(Console.ReadLine());
+
+int SecondDig(int num)
+{
+    return num/10%10;
+}
+
+if (number>999 || number<100) Console.WriteLine("Трехзначное число, please");
+
+else
+{
+    int secondDig=SecondDig(number);
+    Console.WriteLine($"Вторая цифра числа равна - {secondDig}");    
+}
