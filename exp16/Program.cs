f4a640ad83bc3a5b377e442de35941499d8c79a4
LiyaMart <aleka2021@mail.ru>
2023-06-27 18:24:45 +0300

    tast16

---
+++ exp16/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая  принимает на вход цифру, обазначающую день недели, и проверяет, является
+// ли этот день выходным.
+
+Console.Write("Введите цифру ");
+int number=Convert.ToInt32(Console.ReadLine());
+
+bool Weekend(int num)
+{
+   return num==6 || num==7;
+}
+
+bool weekend = Weekend(number);
+Console.WriteLine(weekend ? "Является" : "Не является");
