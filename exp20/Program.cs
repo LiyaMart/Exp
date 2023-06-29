294a242f137d6c571b275cd8e62fe4d664b35c55
LiyaMart <aleka2021@mail.ru>
2023-06-29 20:28:52 +0300

    tast20

---
+++ exp20/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число (N) и выдает таблицу квадратов чисел от 1 до N.
+
+Console.Write("Введите натуральное число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void Quarter(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        Console.Write($"{i * i} ");
+    }
+}
+
+if (n<1) Console.WriteLine("Натуральное чило, please!");
+else Quarter(n);
