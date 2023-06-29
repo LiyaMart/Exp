621ed890760f2cb7570986699b4cb449697011e4
LiyaMart <aleka2021@mail.ru>
2023-06-29 13:03:36 +0300

    tast18

---
+++ exp18/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая по заданному номеру четверти показывает координаты точек в этой четверти (Х, Y)
+
+Console.Write("Введите номер четверти ");
+int quarter=Convert.ToInt32(Console.ReadLine());
+
+string Range(int q)
+{
+    if (q==1) return "х>0 и y>0";
+    if (q==2) return "x<0 и y>0";
+    if (q==3) return "Х<0 и y<0";
+    if (q==4) return "х>0 и y<0";
+    return "Введен некорректный номер четверти";
+}
+
+string range=Range(quarter);
+Console.WriteLine(range);
