30b5c2637aeec32f32213f1f168b72031cf7fce5
LiyaMart <aleka2021@mail.ru>
2023-07-02 14:42:25 +0300

    HW3

---
+++ Homework3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выадет таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите натуральное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void PrintCube(int n)
+{
+   for (int i=1; i<=n; i++)
+    {
+        Console.Write($"{Math.Pow(i,3)} ");
+    }
+}
+
+PrintCube(number);
