d36938b24017b1d92f3d16244d46e8afa3a60966
LiyaMart <aleka2021@mail.ru>
2023-07-02 14:05:54 +0300

    HW1

---
+++ Homework1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вхот пятизначное число и проверяет, является ли оно палидромом.
+
+Console.Write("Введите пятизначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] Array(int n)
+{
+    int [] array = new int[5];
+    int i = array.Length-1;
+    while (i>=0)
+    {
+        array[i] = n%10;
+        n = n/10;
+        i--;
+    }
+    return array;
+} 
+
+bool Poly(int[] arr)
+{
+    return arr[0] == arr[4] && arr[1] == arr[3];
+}
+
+bool poly=Poly(Array(number));
+
+Console.WriteLine(poly);
