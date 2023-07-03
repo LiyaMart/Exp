7ea81e6381574f0bcafe8ae27c4fc5595451f59f
LiyaMart <aleka2021@mail.ru>
2023-07-03 18:39:55 +0300

    exp22

---
+++ exp22/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте массив. Напишите программу, которая отпределяет, присутствует ли заданное число в массиве.
+
+int[] array = {1, 5, 7, 8};
+
+Console.Write("Задайте число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool Choose(int[] a, int n)
+{
+    for (int i = 0; i < a.Length; i++)
+    {
+       if (a[i] == n) return true;        
+    }
+    return false;
+}
+
+Console.Write(Choose(array, number) ? "Присутствует" : "Отсутствует");
